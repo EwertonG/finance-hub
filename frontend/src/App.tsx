3028b8063ddd95372e56cb937cb032da53b60545
EwertonG <ewertonbarcia2007@gmail.com>
2026-08-12 11:48:43 -0300

    feat: initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material';
+
+export function App() {
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4" color="primary" fontWeight="bold">
+        FinanceHub
+      </Typography>
+    </Box>
+  );
+}
+
+export default App;
